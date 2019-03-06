cf6adbab4fff71cf6e309260ebc8341031eabeea
ottavio <ottaviodp@gmail.com>
2019-03-05 22:38:08 -0500

    Update sync.sh
    - Use #!/bin/bash
    
    - Add some output to show progress
    
    - Quote path variables to handle spaces properly.

--- sync.sh
+++ sync.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #
-# Synchronize the contents of this workspace with the corresponding
-# Eden mission folders.
+# Copy the contents thw workspace to the AEden mission folders.
 #
 set -e
 
-MISSIONS="Altis Chernarus Malden sara Stratis Tanoa Takistan Woodland_ACR"
+MISSIONS=`ls ./missions`
 
 if [ -z "$MISSION_PATH" ]; then
     echo "error - MISSION_PATH is not defined"
@@ -18,12 +17,16 @@ if [ "$1" == "--sqm" ]; then
     done
 else
     for i in $MISSIONS; do
-        [ -d "${MISSION_PATH}/CombatDen.${i}" ] || mkdir "${MISSION_PATH}/CombatDen.${i}"
-        cp -ur macros.hpp Description.ext functions/ config/ *.sqf ${MISSION_PATH}/CombatDen.${i}/
-        cp -u missions/${i}/title.jpg ${MISSION_PATH}/CombatDen.${i}
+        printf "${i}...."
 
-        if ! [ -f "${MISSION_PATH}/CombatDen.${i}/mission.sqm" ]; then
-            cp missions/${i}/mission.sqm ${MISSION_PATH}/CombatDen.${i}/
+        [ -d "${MISSION_PATH}"/CombatDen."${i}" ] || mkdir "${MISSION_PATH}"/CombatDen."${i}"
+        cp -ur macros.hpp Description.ext functions/ config/ *.sqf "${MISSION_PATH}"/CombatDen."${i}"/
+        cp -u missions/"${i}"/title.jpg "${MISSION_PATH}"/CombatDen."${i}"
+
+        if ! [ -f "${MISSION_PATH}"/CombatDen."${i}"/mission.sqm ]; then
+            cp missions/"${i}"/mission.sqm "${MISSION_PATH}"/CombatDen."${i}"
         fi
+
+        printf "done\n"
     done
 fi
