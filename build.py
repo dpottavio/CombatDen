2693eb404dff32b989db2798522619ce07ad9f56
ottavio <ottaviodp@gmail.com>
2019-03-20 22:05:59 -0400

    Cleanup build.py

--- build.py
+++ build.py
@@ -13,79 +13,89 @@ import zipfile
 srcFiles = ["config", "functions", "Description.ext", "XEH_preInit.sqf",
             "LICENCE", "initPlayerLocal.sqf", "initServer.sqf", "macros.hpp"]
 
-argParser = argparse.ArgumentParser()
-argParser.add_argument("dest", help="mission destination directory")
-argParser.add_argument("-p", "--pbo", action="store_true", help="build pbo files for each terrain")
-argParser.add_argument("-z", "--zip", action="store_true", help="build pbo files and compress them into a zipfile")
+def main():
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("dest", help="mission destination directory")
+    argParser.add_argument("-p", "--pbo", action="store_true", help="build pbo files for each terrain")
+    argParser.add_argument("-z", "--zip", action="store_true", help="build pbo files and compress them into a zipfile")
 
-args = argParser.parse_args()
+    args = argParser.parse_args()
 
-rootSrcPath         = pathlib.Path(".")
-missionSrcPathBase  = rootSrcPath / "missions"
-destPathBase        = pathlib.Path(args.dest)
+    rootSrcPath        = pathlib.Path(".")
+    missionSrcPathBase = rootSrcPath / "missions"
+    destPathBase       = pathlib.Path(args.dest)
 
-missionSrcList = os.listdir(missionSrcPathBase)
+    missionSrcList = os.listdir(missionSrcPathBase)
 
-pboZip = None
-if args.zip:
-    zipPath = destPathBase / "CombatDen.zip"
-    pboZip = zipfile.ZipFile(str(zipPath), "w", compression=zipfile.ZIP_DEFLATED)
+    pboZip = None
+    if args.zip:
+        zipPath = destPathBase / "CombatDen.zip"
+        pboZip = zipfile.ZipFile(str(zipPath), "w", compression=zipfile.ZIP_DEFLATED)
 
-for terrain in missionSrcList:
-    print (terrain, end="....")
+    for terrain in missionSrcList:
+        print("{:15}".format(terrain), end="")
 
-    missionName = "CombatDen." + terrain
-    missionDestPath = destPathBase / missionName
+        missionName     = "CombatDen." + terrain
+        missionDestPath = destPathBase / missionName
+        missionSrcPath  = missionSrcPathBase / terrain
 
-    if os.path.exists(missionDestPath):
-        shutil.rmtree(missionDestPath)
-
-    missionSrcPath = missionSrcPathBase / terrain
-
-    #
-    # Copy source contents to destination.
-    #
-    shutil.copytree(missionSrcPath, missionDestPath)
-    for src in srcFiles:
-        srcPath  = rootSrcPath / src
-        destPath = missionDestPath / src
-
-        if os.path.isfile(str(src)):
-            shutil.copyfile(srcPath, destPath)
-        else:
-            shutil.copytree(srcPath, destPath)
-
-    #
-    # Add version watermark to Description.ext
-    #
-    try:
-        job = subprocess.run(["git", "describe", "--tags", "--dirty"],
-                             check=True, capture_output=True, text=True)
-
-        version = job.stdout.rstrip()
-        cfgVersion = "\n\nclass CfgVersion { version = \"" + version + "\"; };\n\n"
-
-        descriptionPath = missionDestPath / "Description.ext"
-        descriptionFile = open(str(descriptionPath), "a")
-        descriptionFile.write(cfgVersion)
-    except subprocess.CalledProcessError as e:
-        print ("WARNING: failed to set version watermark:" + str(e))
-
-    if args.zip or args.pbo:
-        # Create PBOs
         try:
-            subprocess.run(["FileBank", str(missionDestPath.resolve())], check=True)
-        except FileNotFoundError as e:
-            print ("ERROR: FileBank was not found. Make sure BI Tools are installed and it's in your path.")
+            if os.path.exists(missionDestPath):
+                shutil.rmtree(missionDestPath)
+
+            #
+            # Copy source contents to destination.
+            #
+            shutil.copytree(missionSrcPath, missionDestPath)
+            for src in srcFiles:
+                srcPath  = rootSrcPath / src
+                destPath = missionDestPath / src
+
+                if os.path.isfile(str(src)):
+                    shutil.copyfile(srcPath, destPath)
+                else:
+                    shutil.copytree(srcPath, destPath)
+        except IOError as e:
+            print("ERROR: " + str(e))
             sys.exit(1)
+        except shutil.SameFileError as e:
+            print("ERROR: source and destination are the same")
+            sys.exit(1)
+
+        #
+        # Add version watermark to Description.ext
+        #
+        try:
+            job = subprocess.run(["git", "describe", "--tags", "--dirty"],
+                                 check=True, capture_output=True, text=True)
+
+            version = job.stdout.rstrip()
+            cfgVersion = "\n\nclass CfgVersion { version = \"" + version + "\"; };\n\n"
+
+            descriptionPath = missionDestPath / "Description.ext"
+            descriptionFile = open(str(descriptionPath), "a")
+            descriptionFile.write(cfgVersion)
         except subprocess.CalledProcessError as e:
-            print ("ERROR: FileBank process:" + str(e))
-            sys.exit(1)
+            print("WARNING: failed to set version watermark:" + str(e))
+
+        if args.zip or args.pbo:
+            # Create PBO
+            try:
+                subprocess.run(["FileBank", str(missionDestPath.resolve())], check=True)
+            except FileNotFoundError as e:
+                rint("ERROR: FileBank was not found. Make sure BI Tools are installed and it's in your path.")
+                sys.exit(1)
+            except subprocess.CalledProcessError as e:
+                print("ERROR: FileBank process:" + str(e))
+                sys.exit(1)
+
+        if pboZip != None:
+            pboZip.write(str(missionDestPath) + ".pbo", arcname=missionName + ".pbo")
+
+        print("ok")
 
     if pboZip != None:
-        pboZip.write(str(missionDestPath) + ".pbo", arcname=missionName + ".pbo")
+        pboZip.close()
 
-    print ("done")
-
-if pboZip != None:
-    pboZip.close()
+if __name__ == "__main__":
+    main()
