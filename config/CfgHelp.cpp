9f76782e21d549e3c515ffb484627cf230f561e6
ottavio <ottaviodp@gmail.com>
2019-12-05 21:52:40 -0500

    Update CfgHelp.cpp.

--- config/CfgHelp.cpp
+++ config/CfgHelp.cpp
@@ -22,12 +22,12 @@ class CfgHelp
     {
         title = "License";
         text  = "Copyright (C) 2018-2019 D. Ottavio <br/><br/>\
-This program is free software: you can redistribute it and/or modify \
+Combat Den is free software: you can redistribute it and/or modify \
 it under the terms of the GNU Lesser General Public License as \
 published by the Free Software Foundation, either version 3 of the \
 License, or (at your option) any later version. \
 <br/><br/>\
-This program is distributed in the hope that it will be useful, \
+Combat Den is distributed in the hope that it will be useful, \
 but WITHOUT ANY WARRANTY; without even the implied warranty of \
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \
 GNU Lesser General Public License for more details. \
